b8180692a529157b910145b2b0c53a14bf23a375
Olegmkv <129410354+Olegmkv@users.noreply.github.com>
2023-10-03 01:42:54 +0300

    finaly

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
+import { User } from './profile/User'
+import  {Gluke}  from './profile/Gluke'
+import  {Statistics}  from './Statistics/Statistics';
+import data from './FriendList/friends.json'
+import { FriendList } from './FriendList/FriendList';
+import listTransaction from './TransactionHistory/transactions.json'
+import{TransactionHistory} from './TransactionHistory/TransactionHistory'
+
+const titleFirst = "Download image"
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <User {...Gluke} />
+      <Statistics title={titleFirst} />
+      <FriendList friend={data} />
+      <TransactionHistory transaction = {listTransaction} />
     </div>
   );
 };
+
